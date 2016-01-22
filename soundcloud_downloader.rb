4c5ea1ea05e3707a2612b032b5af67e742f1e04e
rmalnoult <romainmalnoult@gmail.com>
2016-01-22 12:14:26 +0100

    download a song from any artist on soundcloud

--- soundcloud_downloader.rb
+++ soundcloud_downloader.rb
@@ -2,26 +2,19 @@ require 'soundcloud'
 require "soundcloud-downloader"
 require 'awesome_print'
 require 'colorize'
-# $ gem install colorize
-# $ gem install awesome_print
-# $ gem install soundcloud-downloader
 
 # Ici on utilise une constante (en majuscule) car les constantes sont globales
 # ce qui nous permettra d'appeler notre variable CLIENT depuis n'importe quelle méthode
-CLIENT = SoundClound.new({
-  client_id: "Vdb8f3af04d883b4290ce48092e94d52e",
+CLIENT = SoundCloud.new({
+  client_id: "db8f3af04d883b4290ce48092e94d52e",
   client_secret: "ea13b1094058ce81450d821558a39fc7"
 })
 
 downloader = SoundCloud::Downloader::Client.new({
-  client_id: "Vdb8f3af04d883b4290ce48092e94d52e",
+  client_id: "db8f3af04d883b4290ce48092e94d52e",
   # Le dossier où télécharger les tracks (ex: downloads)
   path: "downloads"
 })
-
-# Affiche les 10 tracks les plus en vogue
-# ap CLIENT.get('/tracks', :limit => 10, :order => 'hotness')
-
 
 def getArtist(name)
 	artists = CLIENT.get('/users', q: name)
@@ -32,24 +25,28 @@ def getTracks(artist_id)
 	return CLIENT.get("/users/#{artist_id}/tracks")
 end
 
+def display_tracks(tracks)
+	i = 0
+	tracks.each do |track|
+		i = i + 1
+		puts i.to_s + ' | ' + track['title']
+	end
+end
+
 puts "Entrez le nom de l'artiste"
 artist_name = gets.chomp
-ap 'name' + artist_name
 
 artist = getArtist(artist_name)
-ap 'artist' + artist
-artist_id = artist["id"]
 
-# renvoie un tableau de tracks sous form de hash
-tracks = getTracks(artist_id)
-ap 'tracks : ' + tracks
-ap tracks[1] # => Un hash contenant toutes les informations sur la track
-ap tracks[1]["title"] # => Nom de la deuxième track
-ap tracks[1]["stream_url"] # => Lien à suivre pour écouter la deuxième track
+tracks = getTracks(artist["id"])
 
-name = ap tracks[1]["title"]
-downloader.download(track_stream_url, { file_name: name, display_progress: true })
+display_tracks(tracks)
 
+puts "Entrez le numero de la track à télécharger"
+track_number = gets.chomp.to_i - 1
 
+filename = tracks[track_number]["user"]["username"] + ' - ' + tracks[track_number]["title"]
+stream_url = tracks[track_number]["stream_url"]
+downloader.download(stream_url, { file_name: filename, display_progress: true })
 
-puts "unestring".orange # Affichera une string de couleur orange dans le terminal
+puts 'All good ! The song ' + tracks[track_number]["title"] + ' by artist ' + tracks[track_number]["user"]["username"] + ' was just downloaded'
